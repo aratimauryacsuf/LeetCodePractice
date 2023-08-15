c204380c17dad6d289b15d571f80ba0aea67f2af
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2023-08-14 22:47:50 -0700

    Time: 2 ms (7.33%), Space: 43.5 MB (34.11%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -16,9 +16,40 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root== null) return false;
+      /* Using DFS 
         if(root.left==null && root.right==null && root.val == targetSum) return true;
         
         return hasPathSum(root.left, targetSum - root.val)|| hasPathSum(root.right,targetSum - root.val);
+        */
+        
+        //Using BFS
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode node =stack.pop();
+            
+            if(node.left == null && node.right == null && node.val == targetSum) return true;
+           //  targetSum = targetSum - node.val;
+            
+            if(node.left != null){
+              //  targetSum = targetSum - node.val;
+                node.left.val = node.left.val + node.val;
+                stack.push(node.left);
+               
+            }
+            
+            
+            if(node.right != null) { 
+                node.right.val = node.right.val + node.val;
+              // targetSum = targetSum - node.val;
+                stack.push(node.right);
+               
+            }
+            
+        }
+        
+        return false;
         
     }
 }
