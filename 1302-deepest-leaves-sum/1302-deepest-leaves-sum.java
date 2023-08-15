187d643db5cc5d0887f50015bae958f224d8f4d3
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2023-08-15 00:18:02 -0700

    Time: 1 ms (100.00%), Space: 44.6 MB (95.86%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -14,8 +14,14 @@
  * }
  */
 class Solution {
+    int maxLevel = 0;
+    int sum =0;
     public int deepestLeavesSum(TreeNode root) {
-     if(root == null) return 0;
+   
+      
+    //Bfs Implementation
+        /*
+        if(root == null) return 0;
         
         int sum =0;
         Queue<TreeNode> q = new LinkedList<>();
@@ -34,11 +40,31 @@ class Solution {
         }
         
         return sum;
-        
+        */
         
         //DFS Implementation
         
+        if(root == null) return 0;
         
+        deepestLeavesSum(root, 0);
+        
+        return sum;
         
     }
+    
+    public void  deepestLeavesSum(TreeNode root, int level){
+        if(root == null) return;
+        
+        if(level > maxLevel){
+            sum =0;
+            maxLevel = level;
+        }
+        if(level == maxLevel){
+            sum = sum + root.val;
+        }
+        
+        deepestLeavesSum(root.left , level+1);
+        deepestLeavesSum(root.right, level +1);
+    }
+
 }
