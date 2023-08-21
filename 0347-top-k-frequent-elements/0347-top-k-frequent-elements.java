4cb83104e7b8762bb694171c73bdae94e3081812
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2023-08-20 23:22:22 -0700

    Time: 13 ms (77.85%), Space: 47.5 MB (81.93%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,38 +1,35 @@
 import java.util.SortedMap;
 class Solution {
     public int[] topKFrequent(int[] a, int k) {
-        int [] res = new int[k];
-        int j =0;
-        Map<Integer,Integer> map = new HashMap<>();
-		SortedMap<Integer, List<Integer>> sortedMap = new TreeMap<>(Collections.reverseOrder());
-
         
-     for(int i = 0; i<a.length; i++){
-         map.put(a[i], map.getOrDefault(a[i], 0)+1);
-     }   
-      for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-         if(sortedMap.containsKey(entry.getValue())){
-             sortedMap.get(entry.getValue()).add(entry.getKey());
-         }
-          else{
-              List<Integer> list = new ArrayList<>();
-              list.add(entry.getKey());
-              sortedMap.put(entry.getValue(), list);
-          }
-          
-      }          
-        for(Map.Entry<Integer, List<Integer>> entry : sortedMap.entrySet()){
-            List<Integer> list = entry.getValue();
-            if(j==k)
-                break;
-            for(int num : list){
-                 res[j++] = num;
-                    if(j == k)
-                         break;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i =0; i< a.length;i++){
+            map.put(a[i],map.getOrDefault(a[i],0)+1);
+        }
+        
+        SortedMap<Integer, List<Integer>> sortedMap = new TreeMap<>(Collections.reverseOrder());
+        
+        for(Map.Entry<Integer,Integer> e: map.entrySet()){
+            if(!sortedMap.containsKey(e.getValue())){
+                sortedMap.put(e.getValue(), new ArrayList<>());
             }
-         
-      }  
+            sortedMap.get(e.getValue()).add(e.getKey());
+        }
+        
+        int[] res = new int[k];
+        int j = 0;
+        
+        for(Map.Entry<Integer,List<Integer>> se : sortedMap.entrySet()){
+            List<Integer> list = se.getValue();
+            if(j==k) break;
+            
+            for(int n : list){
+                res[j++] = n;
+                if(j == k) break;
+            }
+        }
         
         return res;
     }
 }
+       
