7b0edd7dd2348c7db12f301fc2ea8df427a6bea4
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2023-08-23 23:40:52 -0700

    Time: 301 ms (24.95%), Space: 62.2 MB (24.72%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,26 +1,47 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length ==0) return 0;
-        Arrays.sort(nums);
-        int count =0;
-        if(nums.length >0) count =1;
-        for(int i =0; i<nums.length;i++){
-            System.out.print(nums[i] +" ");
-        }
-        System.out.println();
-        int max = 1;
+//         if(nums.length ==0) return 0;
+//         Arrays.sort(nums);
+//         int count =0;
+//         if(nums.length >0) count =1;
+//         for(int i =0; i<nums.length;i++){
+//             System.out.print(nums[i] +" ");
+//         }
+//         System.out.println();
+//         int max = 1;
         
-        for(int i =0; i<nums.length-1;i++){
-            if(nums[i] == nums[i+1]) continue;
-            if(nums[i]  == nums[i+1] - 1 ){
-              count++;  
-            max = Math.max(max,count);
-            System.out.println(count +"is"+ nums[i]);
+//         for(int i =0; i<nums.length-1;i++){
+//             if(nums[i] == nums[i+1]) continue;
+//             if(nums[i]  == nums[i+1] - 1 ){
+//               count++;  
+//             max = Math.max(max,count);
+//             System.out.println(count +"is"+ nums[i]);
+//             }
+//             else{
+//                 max = Math.max(max,count);
+//                 count =1;
+//             }
+//         }
+        
+//         return max;
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int n : nums){
+            set.add(n);
+        }
+        int max=0;
+        
+        for(int n : nums){
+            if(!set.contains(n-1)){
+                int m = n+1;
+                while(set.contains(m)){
+                    m++;
+                }
+                
+                max = Math.max(max,m-n);
             }
-            else{
-                max = Math.max(max,count);
-                count =1;
-            }
+            
         }
         
         return max;
