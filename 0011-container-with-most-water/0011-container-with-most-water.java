5785cdccbdba3309b08a94ff9bd8a33d1229b274
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2023-08-24 23:41:07 -0700

    Time: 5 ms (32.74%), Space: 55.4 MB (73.70%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxArea(int[] h) {
+        
+        // Brute force
+        /*
+        int res =0;
+ 
+        for(int l =0; l< h.length;l++){
+            for(int r =l+1;r<h.length;r++){
+                int area = (r-l)* Math.min(h[l],h[r]);
+                res = Math.max(res,area);
+            }
+        }
+        
+        return res;
+        */
+        
+        // linear 
+        
+        int res =0;
+        int left = 0;
+        int right = h.length -1;
+        
+        while(left < right){
+            int area = (right - left)* Math.min(h[left],h[right]);
+            res = Math.max(res,area);
+            
+            if(h[left] < h[right]) left++;
+            else right--;
+            
+        }
+        
+        
+        return res;
+    }
+}
