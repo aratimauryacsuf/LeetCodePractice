9e3bd92a690c137583adea3a3013ae3c88fe42ae
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2023-08-27 17:20:39 -0700

    Time: 12 ms (78.02%), Space: 44 MB (66.32%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -4,6 +4,7 @@ class Solution {
         
         for(char c : t.toCharArray()){
             map.put(c, map.getOrDefault(c,0)+1);
+          //  System.out.println("c: "+c);
         }
         
         int start =0;
@@ -13,20 +14,28 @@ class Solution {
         
         for(int end=0;end<s.length();end++){
             char right = s.charAt(end);
+            //System.out.println("right: "+ right);
             if(map.containsKey(right)){
                 map.put(right, map.get(right)-1 );
                 if(map.get(right) == 0) matched++;
+                //System.out.println("matched: "+matched);
             }
             
+            //System.out.println("start:"+start +"end:"+ end);
+            
             while(matched == map.size()){
+                //System.out.println("minLen:"+ minLen +"end-start"+ (end-start+1));
                 if(minLen > end - start +1){
                     minLen = end -start+1;
                     subStr = start;
+                   // System.out.println("minLen:"+ minLen +" substr:" + subStr);
                 }
                 
                 char del = s.charAt(start++);
+               // System.out.println("del: "+ del);
                 if(map.containsKey(del)){
                     if(map.get(del)==0) matched--;
+                    //System.out.println("matched at the del:"+matched);
                     map.put(del,map.get(del)+1);
                 }
             }
