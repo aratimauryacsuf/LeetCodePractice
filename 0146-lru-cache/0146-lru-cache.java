ed7457c1a242fd805097d5a45b027b183cc083ea
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2023-09-05 13:33:04 -0700

    Time: 843 ms (5.00%), Space: 118.7 MB (75.38%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -25,28 +25,18 @@ class LRUCache {
     }
     
     public void put(int key, int value) {
-        if(map.size() == capacity && !(map.containsKey(key))){
-           
-            map.remove(q.pollLast());
+        if(map.containsKey(key)){
             map.put(key,value);
+            q.remove(key);
             q.addFirst(key);
            // System.out.println("check capacity Map" + map);
         }else{
-            if(!map.containsKey(key)){
-                 q.addFirst(key);
-                map.put(key, value);
-               // System.out.println("update Map" + map);
+            if(map.size() == capacity){
+                map.remove(q.pollLast());
             }
-            else{
-                q.remove(key);
-                q.addFirst(key);
-                map.put(key, value);
-               // System.out.println("new put Map" + map);
-            }
-            
-            
+            q.addFirst(key);
+            map.put(key, value); 
         }        
-     //    System.out.println("Put Map" + map);
     }
     
     
