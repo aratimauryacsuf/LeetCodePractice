2e84fc70c7779c8afc616a2265e2a4df03a675f5
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2023-09-05 00:10:05 -0700

    Time: 20 ms (33.70%), Space: 60.7 MB (5.62%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -5,16 +5,19 @@ class Solution {
             if(!map.containsKey(nums[i])){
                 map.put(nums[i], 1);
             }else{
-                map.put(nums[i], map.get(nums[i])+1);
+               // map.put(nums[i], map.get(nums[i])+1);
+                
+                return nums[i];
             }
         }
-        int num =-1;
+       /* int num =-1;
         for(int i=0; i<nums.length; i++){
             if(map.get(nums[i]) > 1){
                  num = nums[i];
             }
         }
+        */
         
-        return num;
+        return -1;
     }
 }
