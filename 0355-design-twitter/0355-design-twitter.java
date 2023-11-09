15a14b83f673802a2bc1ad24e1b0fda106718c15
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2023-11-09 00:27:54 -0800

    Time: 9 ms (91.95%), Space: 40.3 MB (83.85%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.java
+++ 0355-design-twitter/0355-design-twitter.java
@@ -32,7 +32,7 @@ class Twitter {
             if(tweetMap.containsKey(followeeId)){
                 List<int[]> followeeTweets = tweetMap.get(followeeId);
                 int size = followeeTweets.size();
-                System.out.println("in getfeed  no of tweet"+ size);
+                //System.out.println("in getfeed  no of tweet"+ size);
                 if (size > 0) {
                     int[] tweet = followeeTweets.get(size - 1);
                     pq.offer(new int[]{tweet[0], tweet[1], followeeId, size - 1});
@@ -43,7 +43,7 @@ class Twitter {
         
         while(!pq.isEmpty() && res.size() <10){
             int[] data = pq.poll();
-            System.out.println("count:"+ data[0]+ " tweetid"+ data[1]);
+            //System.out.println("count:"+ data[0]+ " tweetid"+ data[1]);
             res.add(data[1]);
             
             int followeeId = data[2];
@@ -51,7 +51,7 @@ class Twitter {
             
             if(tweetIndex >0){
                 int[] tweet = tweetMap.get(followeeId).get(tweetIndex-1);
-                System.out.println("in tweetindex:"+ tweet[0] +" ID:"+ tweet[1]);
+               // System.out.println("in tweetindex:"+ tweet[0] +" ID:"+ tweet[1]);
                  pq.offer(new int[]{tweet[0], tweet[1], followeeId, tweetIndex - 1});
             }
         }
