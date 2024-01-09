e4ddcf1660fca9b431a2c9698db78430f6dbff5c
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-01-09 12:10:28 -0800

    Time: 8 ms (50.46%), Space: 56.9 MB (22.86%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> res = new ArrayList<>();
-        List<String> list = new ArrayList<>();
+        List<String> list  = new ArrayList<>();
         
         backtrack(0, s, res, list);
         return res;
@@ -9,21 +9,23 @@ class Solution {
     
     public void backtrack(int start, String s, List<List<String>> res, List<String> list){
         
+        
+        
         if(start >= s.length()){
             res.add(new ArrayList<>(list));
         }
+        
         for(int end = start; end < s.length(); end++){
             if(palindrome(s, start, end)){
                 list.add(s.substring(start, end+1));
                 backtrack(end+1, s, res, list);
-                list.remove(list.size() -1);
+                list.remove(list.size()-1);
             }
-            
         }
     }
     
     public boolean palindrome(String s, int start, int end){
-        while (start < end){
+        while(start<end){
             if(s.charAt(start++) != s.charAt(end--)) return false;
         }
         return true;
