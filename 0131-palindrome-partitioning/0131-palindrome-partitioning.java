3cfdf0ad7463bfb316f804da13ed50b3239f581b
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-01-09 12:19:01 -0800

    Time: 7 ms (77.45%), Space: 57.3 MB (11.94%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -9,17 +9,23 @@ class Solution {
     
     public void backtrack(int start, String s, List<List<String>> res, List<String> list){
         
-        
+       // System.out.println("res at start of backtrack: "+  res);
+       // System.out.println("start: "+  start);
         
         if(start >= s.length()){
+           // System.out.println("in base case check: "+  res);
             res.add(new ArrayList<>(list));
+            // System.out.println("in base case check after: "+  res);
         }
         
         for(int end = start; end < s.length(); end++){
+           // System.out.println("end value: "+  end);
             if(palindrome(s, start, end)){
                 list.add(s.substring(start, end+1));
+               // System.out.println("list in for loop: "+  list);
                 backtrack(end+1, s, res, list);
                 list.remove(list.size()-1);
+              //  System.out.println("list loop:  end of"+  list);
             }
         }
     }
