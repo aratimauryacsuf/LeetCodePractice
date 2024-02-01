e93d7e1c3d92c7238c060ae65b80a30d7bc818d6
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-01 00:28:34 -0800

    Time: 2 ms (76.51%), Space: 45.3 MB (35.29%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int jump(int[] nums) {
+        int count=0, curEnd =0, curFar =0;
+        
+        for(int i=0; i< nums.length -1; i++){
+            curFar = Math.max(curFar, i+nums[i]);
+            if(i == curEnd){
+                count++;
+                curEnd = curFar;
+            }
+        }
+        return count;
+    }
+}
