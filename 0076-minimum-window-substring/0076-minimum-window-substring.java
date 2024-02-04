014ac50ea7092a079e3f192801319f5ce32616c3
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-03 23:17:26 -0800

    Time: 75 ms (16.71%), Space: 45.9 MB (7.49%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,6 +1,6 @@
 class Solution {
     public String minWindow(String s, String t) {
-       
+      /* 
         Map<Character, Integer> map = new HashMap<>();
         
         for(char c : t.toCharArray()){
@@ -34,5 +34,44 @@ class Solution {
         }
         
         return minLen>s.length()?"" : s.substring(subStr, subStr+minLen);
+    */
+        String result ="";
+        if(s.length() == 0 || t.length() ==0) return result;
+        int need = t.length();
+        int have = 0;
+        int left =0;
+        int minWindow =Integer.MAX_VALUE;
+        HashMap<Character, Integer> mapt = new HashMap<>();
+        HashMap<Character, Integer> mapS = new HashMap<>();
+        for(int i =0; i < t.length() ; i++){
+            mapt.put(t.charAt(i), mapt.getOrDefault(t.charAt(i), 0)+1);
+        }
+        int start =0;
+       for(int end =0; end < s.length(); end++){
+           mapS.put(s.charAt(end),mapS.getOrDefault(s.charAt(end), 0)+1);
+           if(mapt.containsKey(s.charAt(end)) && mapS.get(s.charAt(end)) <=
+           mapt.get(s.charAt(end))){
+               have++;
+           }
+
+           while(have == need){
+               if(minWindow > end - start +1){
+                   minWindow = end - start +1;
+                   result = s.substring(start,end +1);
+               }
+               char removeCh = s.charAt(start);
+               if(mapS.get(removeCh) == 1){
+                   mapS.remove(removeCh);
+               }else{
+                   mapS.put(removeCh, mapS.get(removeCh)-1);
+               }
+               start++;
+               if(mapt.containsKey(removeCh) && mapS.getOrDefault(removeCh,0) < (mapt.get(removeCh))){
+                   have--;
+               }
+           }
+       }
+
+        return result;
     }
 }
