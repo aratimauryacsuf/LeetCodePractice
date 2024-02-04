d8c3bc86807dd35eecd61a6005c67ce1bd8ab952
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-03 23:44:09 -0800

    Time: 0 ms (100.00%), Space: 41.7 MB (15.00%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -2,15 +2,15 @@ class Solution {
     public boolean checkValidString(String s) {
         int lo = 0, hi = 0;
        for (char c: s.toCharArray()) {
-            System.out.println("C: "+ c);
+            //System.out.println("C: "+ c);
            lo += c == '(' ? 1 : -1;
-           System.out.println("lo: "+ lo);
+           //System.out.println("lo: "+ lo);
            hi += c != ')' ? 1 : -1;
            
-System.out.println("hi: "+ hi);
+//System.out.println("hi: "+ hi);
            if (hi < 0) break;
            lo = Math.max(lo, 0);
-          System.out.println("loMax: "+ lo);  
+          //System.out.println("loMax: "+ lo);  
        }
        return lo == 0;
     }
