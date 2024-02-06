7b50169a4213ee2271a7db04766616965208eacc
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-06 13:04:36 -0800

    Time: 7 ms (58.01%), Space: 47.6 MB (44.86%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String , List<String>> map = new HashMap<>();
+        //List<List<string>> = new ArrayList<>();
         
-    if(strs == null || strs.length == 0) return new ArrayList<>();
-        
-    Map<String, List<String>> map = new HashMap<>();
-    
-    for(String s: strs){
-        char[] ca = new char[26];
-        for(char c: s.toCharArray()) ca[c -'a']++; 
-        String strKey = String.valueOf(ca);
-        if(!map.containsKey(strKey)) map.put(strKey, new ArrayList<>());
-        map.get(strKey).add(s);
-}
+        for(String s: strs){
+            char c[] = new char[26];
+            for(Character ch : s.toCharArray()){
+                c[ch-'a']++;
+            }
+            
+            String str = new String(c);
+            
+            if(!map.containsKey(str)){
+                map.put(str, new ArrayList<>());
+            }
+            map.get(str).add(s);
+        }
         
         return new ArrayList<>(map.values());
     }
