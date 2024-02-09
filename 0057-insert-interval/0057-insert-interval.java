2c9a019dc0994b34ca5597e483cc22099b46e521
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-08 23:29:31 -0800

    Time: 1 ms (99.19%), Space: 44.7 MB (69.50%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,25 +1,30 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> output = new ArrayList<>();
-
-        for(int[] i : intervals){
-            if(newInterval == null || i[1] < newInterval[0]){
-                output.add(i) ;
-            }else if(i[0] > newInterval[1]){
-                    output.add(newInterval);
-                    output.add(i);
-                    newInterval = null;
-
-                }
-            else{
-                newInterval[0] = Math.min(newInterval[0], i[0]);
-                newInterval[1] = Math.max(newInterval[1], i[1]);
-                }
-        }
-        if(newInterval != null)
-         output.add(newInterval);
         
-
-        return output.toArray(new int[output.size()][]) ;
+       // if(intervals.length ==0) return newInterval;
+        
+      List<int[]> ans = new ArrayList<>();
+        
+        for(int[] i : intervals){
+            if(newInterval == null || i[1]< newInterval[0]){
+                ans.add(i);
+            }else if(i[0] > newInterval[1]){
+                ans.add(newInterval);
+                ans.add(i);
+                newInterval = null;
+            }
+            else{
+                newInterval[0] = Math.min(i[0], newInterval[0]);
+                newInterval[1] = Math.max(i[1], newInterval[1]);
+            }
+            
+            
+        }
+        if(newInterval != null){
+                ans.add(newInterval);
+                newInterval = null;
+            }
+        
+        return ans.toArray(new int[ans.size()][]);
     }
 }
