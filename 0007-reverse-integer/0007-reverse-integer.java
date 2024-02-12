3da8aca0c37111ba7248c6fa9130301126f3a384
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-11 23:03:29 -0800

    Time: 1 ms (90.35%), Space: 40.8 MB (46.16%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -19,10 +19,13 @@ class Solution {
          int rev = 0;
         while (x != 0) {
             int pop = x % 10;
+            //System.out.println("Pop: "+ pop);
             x /= 10;
+            //System.out.println("X: "+ x);
             if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
             if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
             rev = rev * 10 + pop;
+            //System.out.println("rev: "+ rev);
         }
         return rev;
     
