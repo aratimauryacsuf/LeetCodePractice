a0df5bfb2a4ac4a694d4390eca1ac8d13c221d9a
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-11 20:19:25 -0800

    Time: 1 ms (99.82%), Space: 50.7 MB (29.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // int major= nums[0], count = 1;
-        // for(int i =1; i<nums.length; i++){
-        //     if(count == 0){
-        //         count++;
-        //         major = nums[i];
-        //     }else if(major == nums[i]){
-        //         count++;
-        //     }else{
-        //         count--;
-        //     }
-        // }
-        // return major;
+        //moore venting algorithm
+        int major= nums[0], count = 1;
+        for(int i =1; i<nums.length; i++){
+            if(count == 0){
+                count++;
+                major = nums[i];
+            }else if(major == nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return major;
         
         //Sorting
-        Arrays.sort(nums);
-    return nums[nums.length/2];
+    //     Arrays.sort(nums);
+    // return nums[nums.length/2];
     }
 }
