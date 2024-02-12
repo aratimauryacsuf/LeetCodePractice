222173f0a286c3cc768dc694025869dffd9cb54b
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-11 22:58:43 -0800

    Time: 1 ms (90.35%), Space: 40.3 MB (87.76%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,6 +1,6 @@
 class Solution {
     public int reverse(int x) {
-       long rev =0;
+       /*long rev =0;
         while(x != 0){
             rev+= (x % 10);
             rev = rev *10;
@@ -15,5 +15,16 @@ class Solution {
         }
         
         return (int)rev;
+        */
+         int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    
     }
 }
