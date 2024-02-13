a0872d4040fe2761cc24b1d6c94817c416d2eb21
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-12 20:20:14 -0800

    Time: 1 ms (100.00%), Space: 44.8 MB (65.40%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public String firstPalindrome(String[] words) {
-        
+     
+        /* Method 1
         for(int i =0 ; i< words.length ;i++){
             String rev = "";
             String str = words[i];
@@ -10,12 +11,31 @@ class Solution {
             rev = rev + str.charAt(j);
         }
  
-        // Checking if both the strings are equal
+        
         if (str.equals(rev)) {
             return words[i];
         }
         }
         
         return "";
+        */
+        
+         for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+
+    private boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
