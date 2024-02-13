9539ffa5d53b4c45f3b2f12f945c5c38808fc4bb
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-13 15:22:33 -0800

    Time: 6 ms (56.67%), Space: 121.8 MB (42.71%) - LeetHub

---
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean mergeTriplets(int[][] ts, int[] tar) {
+        int[] res = new int[tar.length];
+        
+        for(int[] t: ts){
+           
+            if(t[0] <= tar[0] && t[1] <= tar[1] && t[2] <= tar[2]){
+                res[0] = Math.max(t[0], res[0]);
+                res[1] = Math.max(t[1], res[1]);
+                res[2] = Math.max(t[2], res[2]);
+                
+                //or this one line
+                // res = new int[]{ Math.max(res[0], s[0]), Math.max(res[1], s[1]), Math.max(res[2], s[2]) };
+            }
+        }
+            
+        
+    
+        return Arrays.equals(res,tar);
+    }
+}
