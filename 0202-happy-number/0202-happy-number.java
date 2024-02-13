06c63d74054aff29eabce03099e3742a49323bb0
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-12 21:42:19 -0800

    Time: 1 ms (71.74%), Space: 40.9 MB (32.21%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,33 +1,40 @@
 class Solution {
     public boolean isHappy(int n) {
-        if(n ==0 ) return false;
+        if(n == 0) return false;
+        
         Set<Integer> set = new HashSet<>();
+        
         int sum = sumOfNum(n);
         
-        while(sum > 0){
+       // System.out.println("main sum: "+ sum );
+        
+        while(sum >0){
             sum = sumOfNum(sum);
-            
+            //System.out.println("in while sum: "+ sum );
             if(sum == 1){
                 return true;
-            }else{
-                if(!set.contains(sum)){
-                   set.add(sum); 
-                }else return false;
+            }else if(!set.contains(sum)){
+                set.add(sum);
                 
+            }else{
+                return false;
             }
         }
         
-      return false;  
+        return false;
     }
     
-    public int sumOfNum(int n ){
-        int sum =0 ;
-        
-        while(n > 0){
-            sum = sum + (n %10 )* (n % 10);
-            n = n / 10;
-        }
-        
-        return sum;
-    }
+     public int sumOfNum(int n){
+         int sum = 0;
+         
+         while(n >0){
+             sum = sum + (n%10) * (n%10) ;
+            // System.out.println("Sum in helper function: "+ sum );
+             n = n/10;
+             //System.out.println("n: "+ n );
+         }
+         
+         
+         return sum;
+     }
 }
