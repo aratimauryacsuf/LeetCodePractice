ab6a738f9698c6f5a65d80ea005242d07de7b4ea
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-13 22:28:57 -0800

    Time: 3 ms (100.00%), Space: 77 MB (49.34%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int[] positive = new int[nums.length / 2];
+        
+        /*int[] positive = new int[nums.length / 2];
         int[] negative = new int[nums.length /2];
         int pIdx =0;
         int nIdx =0;
@@ -25,5 +26,21 @@ class Solution {
         }
         
         return result;
+        */
+        
+        // optimized Version 
+        int [] out = new int [nums.length];
+        int j = 0;
+        int k = 1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] >= 0){
+                out[j] = nums[i];
+                j += 2;
+            }else {
+                out[k] = nums[i];
+                k += 2;
+            }
+        }
+        return out;
     }
 }
