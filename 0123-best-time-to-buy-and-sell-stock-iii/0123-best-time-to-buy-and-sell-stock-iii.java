cd6cc415460c9f7f95224ecc80ac6b1e4080a7f9
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-15 15:19:47 -0800

    Time: 3 ms (82.79%), Space: 61.3 MB (43.07%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -31,7 +31,7 @@ class Solution {
 		return sell2;
        */
         
-        
+       /* way 2 
         int t1Cost = Integer.MAX_VALUE, 
         t2Cost = Integer.MAX_VALUE;
     int t1Profit = 0,
@@ -47,5 +47,32 @@ class Solution {
     }
 
     return t2Profit;
+    */
+        int length = prices.length;
+    if (length <= 1) return 0;
+
+    int leftMin = prices[0];
+    int rightMax = prices[length - 1];
+
+    int[] leftProfits = new int[length];
+    // pad the right DP array with an additional zero for convenience.
+    int[] rightProfits = new int[length + 1];
+
+    // construct the bidirectional DP array
+    for (int l = 1; l < length; ++l) {
+      leftProfits[l] = Math.max(leftProfits[l - 1], prices[l] - leftMin);
+      leftMin = Math.min(leftMin, prices[l]);
+
+      int r = length - 1 - l;
+      rightProfits[r] = Math.max(rightProfits[r + 1], rightMax - prices[r]);
+      rightMax = Math.max(rightMax, prices[r]);
+    }
+
+    int maxProfit = 0;
+    for (int i = 0; i < length; ++i) {
+      maxProfit = Math.max(maxProfit, leftProfits[i] + rightProfits[i + 1]);
+    }
+    return maxProfit;
+
     }
 }
