b2274d61e6200ade2760cf12fc3d6f4972bb3eaf
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-15 15:18:26 -0800

    Time: 1 ms (99.98%), Space: 60.9 MB (61.60%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -15,14 +15,37 @@ class Solution {
 //         }
 //         return profit;
         
-        
+   /* way 1     
         int sell1 = 0, sell2 = 0, buy1 = Integer.MIN_VALUE, buy2 = Integer.MIN_VALUE;
 		for (int i = 0; i < prices.length; i++) {
+            System.out.println("Stock when i =: " + prices[i]);
 			buy1 = Math.max(buy1, -prices[i]);
+            System.out.println("buy1: " + buy1);
 			sell1 = Math.max(sell1, buy1 + prices[i]);
+            System.out.println("sell1: " + sell1);
 			buy2 = Math.max(buy2, sell1 - prices[i]);
+            System.out.println("buy2: " + buy2);
 			sell2 = Math.max(sell2, buy2 + prices[i]);
+            System.out.println("sell2: " + sell2);
 		}
 		return sell2;
+       */
+        
+        
+        int t1Cost = Integer.MAX_VALUE, 
+        t2Cost = Integer.MAX_VALUE;
+    int t1Profit = 0,
+        t2Profit = 0;
+
+    for (int price : prices) {
+        // the maximum profit if only one transaction is allowed
+        t1Cost = Math.min(t1Cost, price);
+        t1Profit = Math.max(t1Profit, price - t1Cost);
+        // reinvest the gained profit in the second transaction
+        t2Cost = Math.min(t2Cost, price - t1Profit);
+        t2Profit = Math.max(t2Profit, price - t2Cost);
+    }
+
+    return t2Profit;
     }
 }
