bbd983510f72bc045dda6431b42728c8bff2708d
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-16 11:10:24 -0800

    Time: 40 ms (44.78%), Space: 51.5 MB (50.36%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -7,6 +7,8 @@ class Solution {
         Arrays.sort(nums);
         
         for(int i =0; i< nums.length-2;i++){
+            if(i ==0 || nums[i] != nums[i-1]){
+                
             
           int  left = i+1;
            int right = nums.length-1;
@@ -18,6 +20,7 @@ class Solution {
                 }else if(sum >0) right--;
                 else if(sum < 0) left++;
             }
+        }
         }
         
         return new ArrayList<>(result);
