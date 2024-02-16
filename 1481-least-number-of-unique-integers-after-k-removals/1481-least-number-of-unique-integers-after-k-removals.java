3ca74568e7d340bd90365fc66b646e6b45846e7d
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-15 16:27:06 -0800

    Time: 47 ms (73.49%), Space: 57.2 MB (67.57%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
+      /*  Map<Integer, Integer> map = new HashMap<>();
         
         for(int i=0; i< arr.length; i++){
             map.put(arr[i], map.getOrDefault(arr[i],0)+1);
@@ -15,7 +15,20 @@ class Solution {
         }
         
         return k<0? pq.size()+1: pq.size();
-        
-        
+        */
+         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i : arr)
+            map.put(i, map.getOrDefault(i, 0)+1);
+        List values= new ArrayList(map.values());
+        Collections.sort(values);
+        int count = values.size();
+        for(Object a : values){
+            if((int)a <= k){
+                k -= (int)a;
+                count--;
+            }
+            else if(k < (int)a) break;
+        }
+        return count;
     }
 }
