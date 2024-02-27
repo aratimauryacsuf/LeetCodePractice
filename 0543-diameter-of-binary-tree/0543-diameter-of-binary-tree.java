e99870f43e2f979d6f190cd03fd84885ec2ed1ec
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-26 19:56:33 -0800

    Time: 28 ms (5.34%), Space: 44.5 MB (5.57%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -24,10 +24,13 @@ class Solution {
     
     public int maxDepth(TreeNode root){
         if(root == null) return 0;
-        
+        //System.out.println("root: "+root.val);
         int left =maxDepth(root.left);
+       // System.out.println("root.right:");
         int right = maxDepth(root.right);
+        //System.out.println("root: " + root.val+" left: "+ left + " right: "+ right);
         max =Math.max(max, left+right);
+        System.out.println("max:" +max);
         
         return Math.max(left,right)+1;
     }
