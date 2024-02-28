bc69a2e3bcd792c29c8b7559d59feaea31b0de22
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-02-27 20:48:13 -0800

    Time: 9 ms (30.87%), Space: 44.1 MB (54.27%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,11 +1,11 @@
 class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
+    public boolean wordBreak(String s, List<String> wordDict1) {
+        Set<String> wordDict = new HashSet<>(wordDict1);
         int len = s.length();
         int sol[] = new int[len+1];
         
         for(int i = 0; i <= len; i++){
             String sub = s.substring(0, i);
-            //System.out.println("outer: " + sub);
             if(wordDict.contains(sub)){
                 sol[i] = 1;
             }
@@ -13,7 +13,6 @@ class Solution {
             if(sol[i] == 1){
                 for(int j = i; j <= len; j++){
                     String sub2 = s.substring(i, j);
-                 //   System.out.println("inner: " + sub2);
                      if(wordDict.contains(sub2)){
                             sol[j] = 1;
                         }
