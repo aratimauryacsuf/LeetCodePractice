8ca588cf36c71e6ea97bcfccd36c2ed3ef3185b7
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-03-06 20:17:54 -0800

    Time: 11 ms (63.27%), Space: 44.2 MB (36.46%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,6 +1,8 @@
-class Solution {
+//Using map
+
+/*class Solution {
    // int count =0;
-      Map<String, Integer> cache = new HashMap<>();
+     // Map<String, Integer> cache = new HashMap<>();
     public int findTargetSumWays(int[] nums, int target) {
           
         return backtrack(0, 0, nums, target);
@@ -29,3 +31,38 @@ class Solution {
         return cache.get(key);
     }
 }
+*/
+
+// using arrays
+public class Solution {
+    int total;
+    
+    public int findTargetSumWays(int[] nums, int S) {
+        total = Arrays.stream(nums).sum();
+        
+        int[][] memo = new int[nums.length][2 * total + 1];
+        for (int[] row : memo) {
+            Arrays.fill(row, Integer.MIN_VALUE);
+        }
+        return calculate(nums, 0, 0, S, memo);
+    }
+    
+    public int calculate(int[] nums, int i, int sum, int S, int[][] memo) {
+        if (i == nums.length) {
+            if (sum == S) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (memo[i][sum + total] != Integer.MIN_VALUE) {
+                return memo[i][sum + total];
+            }
+            int add = calculate(nums, i + 1, sum + nums[i], S, memo);
+            int subtract = calculate(nums, i + 1, sum - nums[i], S, memo);
+            memo[i][sum + total] = add + subtract;
+            return memo[i][sum + total];
+        }
+    }
+}
+
