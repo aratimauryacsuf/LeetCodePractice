f31be232ff3646ac4fe5fbd4297df724d22df395
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-03-14 00:02:03 -0700

    Time: 3 ms (69.27%), Space: 47.8 MB (56.52%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -10,21 +10,30 @@ class Solution {
             // Add current element to the sum
             currentSum += nums[end];
             
+           // System.out.println("currenSum befor while: "+ currentSum);
+            
             // Slide the window while condition is met
             while (start < end && (nums[start] == 0 || currentSum > goal)) {
+             //  System.out.println("Start: "+ start);
                 if (nums[start] == 1) {
                     prefixZeros = 0;
                 } else {
                     prefixZeros++;
                 }
                 
+               // System.out.println("PRefixZeros: "+ prefixZeros);
                 currentSum -= nums[start];
+               // System.out.println("currenSum Inside  while: "+ currentSum);
                 start++;
             }
             
+           // System.out.println("currenSum After while: "+ currentSum);
+            
             // Count subarrays when window sum matches the goal
             if (currentSum == goal) {
+                
                 totalCount += 1 + prefixZeros;
+             //   System.out.println("totalcount: "+ totalCount);
             }
         }
         
