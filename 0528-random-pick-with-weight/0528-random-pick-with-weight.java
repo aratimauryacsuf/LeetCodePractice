081b3f2e0d6cbfad86ffe144516fb37dd1e7d5f3
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-04-03 18:31:16 -0700

    Time: 78 ms (18.08%), Space: 49.9 MB (32.99%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    int[] runningSum;
+    public Solution(int[] w) {
+        runningSum = new int[w.length];
+        int sum = 0;
+        for(int i = 0; i<w.length; i++){
+            runningSum[i] = sum + w[i];
+            sum = runningSum[i];
+        }
+    }
+    
+    public int pickIndex() {
+        int sum = runningSum[runningSum.length-1];
+        //System.out.println("sum: "+ sum);
+        int random = getRandomNumber(0,sum)+1;//(int)(Math.random()*sum);
+       // System.out.println("random: "+ random);
+        for (int i = 0; i < runningSum.length; i++){
+            if(random <= runningSum[i])
+                return i;
+        }
+        return 0;
+    }
+    public int getRandomNumber(int min, int max) {
+    return (int) ((Math.random() * (max - min)) + min);
+}
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
