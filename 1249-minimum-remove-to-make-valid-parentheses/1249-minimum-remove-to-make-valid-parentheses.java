549ffc2d94919b923a548bb6e1f861065ae708d6
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-04-06 00:56:43 -0700

    Time: 11 ms (91.86%), Space: 45.3 MB (70.43%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,5 +1,6 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
+      /*
         Stack<Integer> stack = new Stack<>();
         HashSet<Integer> todoRemove = new HashSet<>();
        
@@ -26,5 +27,26 @@ class Solution {
         }
         
         return new String(sb);
+        */
+        
+        int openCloseCount = 0;
+		int close = 0;
+		for (int i = 0; i < s.length(); i++)  if (s.charAt(i) == ')') close++;
+		
+        StringBuilder sb = new StringBuilder();
+		
+		for (char c: s.toCharArray()) {
+			if (c == '(') {
+				if (openCloseCount == close) continue;
+				openCloseCount++;
+			} else if (c == ')') {
+				close--;
+				if (openCloseCount == 0) continue;
+				openCloseCount--;
+			} 
+
+				sb.append(c);
+		}
+		return sb.toString();
     }
 }
