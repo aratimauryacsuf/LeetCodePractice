3d5c599485a5c063f114d200c9298ef5ef2b6d5a
Arati Maurya <112656100+aratimauryacsuf@users.noreply.github.com>
2024-04-06 00:55:00 -0700

    Time: 25 ms (38.22%), Space: 45.4 MB (41.40%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,24 +1,30 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack<Integer> openSt = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         HashSet<Integer> todoRemove = new HashSet<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(')
-                openSt.push(i);
-            else if (s.charAt(i) == ')') {
-                if (openSt.isEmpty())
-                    todoRemove.add(i); // Meet closing-parentheses but no opening-parenthesis -> remove closing-parenthesis 
-                else
-                    openSt.pop();
+       
+        for(int i =0 ; i< s.length(); i++){
+            
+            if(s.charAt(i) == '('){
+                stack.push(i);
+            }else if(s.charAt(i) == ')'){
+                if(stack.isEmpty()){
+                    todoRemove.add(i);
+                }else{
+                    stack.pop();
+                }
             }
         }
-        todoRemove.addAll(openSt); // remove remain opening-parenthesis
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < s.length(); i++)
-            if (!todoRemove.contains(i))
-                stringBuilder.append(s.charAt(i));
-
-        return stringBuilder.toString();
+        
+        todoRemove.addAll(stack);
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i =0; i< s.length() ; i++){
+            if(!todoRemove.contains(i)){
+                sb.append(s.charAt(i));
+            }
+        }
+        
+        return new String(sb);
     }
 }
